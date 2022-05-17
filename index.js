e809b1e27b4b995f174e5ea69a33620da8d86628
yu-hiramoto <yu_hiramoto@mail.toyota.co.jp>
2022-05-17 10:51:08 +0900

    second commit

--- index.js
+++ index.js
@@ -1,140 +1,74 @@
-// console.log("hello");
 class WorldWeather {
-  constructor(cityName) {
+  constructor() {
     this.hourlyTime = [];
     this.weatherCode = [];
     this.weatherCodeObject = {
-      0: "晴天",
-      1: "晴れ",
-      2: "一部曇り",
-      3: "曇り",
-      45: "霧",
-      48: "濃い霧",
-      51: "霧雨",
-      53: "霧雨",
-      55: "霧雨",
-      56: "霧雨",
-      57: "霧雨",
-      61: "弱い雨",
-      63: "雨",
-      65: "強い雨",
+      0: "晴天🌤",
+      1: "晴れ🌤",
+      2: "一部曇り⛅️",
+      3: "曇り☁︎",
+      45: "霧🌫",
+      48: "濃い霧🌫",
+      51: "霧雨🌂",
+      53: "霧雨🌂",
+      55: "霧雨🌂",
+      56: "霧雨🌂",
+      57: "霧雨🌂",
+      61: "弱い雨🌂",
+      63: "雨☔️",
+      65: "強い雨☔️",
       66: "霰",
       67: "霰",
-      71: "弱い雪",
-      73: "雪",
-      75: "強い雪",
-      99: "重度のひょうを伴う雷雨",
-      96: "軽度のひょうを伴う雷雨",
-      95: "雷雨",
-      86: "重度のにわか雪",
-      85: "軽度のにわか雪",
-      82: "重度のにわか雨",
-      81: "中度のにわか雨",
-      80: "経度のにわか雨",
-      77: "霧雪",
+      71: "弱い雪⛄️",
+      73: "雪⛄️",
+      75: "強い雪⛄️",
+      99: "重度のひょうを伴う雷雨⛄️⚡️",
+      96: "軽度のひょうを伴う雷雨⛄️⚡️",
+      95: "雷雨☔️⚡️",
+      86: "重度のにわか雪⛄️",
+      85: "軽度のにわか雪⛄️",
+      82: "重度のにわか雨☔️⚡️",
+      81: "中度のにわか雨🌂",
+      80: "軽度のにわか雨🌂",
+      77: "霧雪⛄️",
     };
-    this.cityName = cityName;
     this.city = {
-      // "Tokyo":latitude=35.6785&longitude=139.6823,
       Tokyo: [35.6785, 139.6823],
       "New York": [40.71, -74.01],
+      London: [51.5002, -0.1262],
+      "North Pole": [90, 0],
+      "Sauth Pole": [-90, 0],
     };
   }
-
   async searchWeather() {
-    await axios
-      .get(
-        // `https://api.open-meteo.com/v1/forecast?${this.city[]}latitude=35.6785&longitude=139.6823&hourly=temperature_2m,weathercode`
-        `https://api.open-meteo.com/v1/forecast?latitude=${
-          this.city[this.cityName][0]
-        }&longitude=${
-          this.city[this.cityName][1]
-        }&hourly=temperature_2m,weathercode`
-      )
-      .then((data) => {
-        //console.log(this.hourlyTime);
-        this.hourlyTime = data.data.hourly.time;
-        this.weatherCode = data.data.hourly.weathercode;
-        // console.log(this.hourlyTime);
-        // console.log(this.weatherCode);
-        this.weatherCreatElement();
-      });
+    for (const key of Object.keys(this.city)) {
+      await axios
+        .get(
+          `https://api.open-meteo.com/v1/forecast?latitude=${this.city[key][0]}&longitude=${this.city[key][1]}&hourly=temperature_2m,weathercode`
+        )
+        .then((data) => {
+          this.hourlyTime = data.data.hourly.time;
+          this.weatherCode.push(data.data.hourly.weathercode);
+        });
+    }
+    this.weatherCreatElement();
   }
-  // serchWeather();
 
   weatherCreatElement() {
-    // const selectCity = document.getElementById("selectCity");
-    // console.log(selectCity);
-    // selectCity.addEventListener("change", function (event) {
-    //   console.log("hello");
-    //   console.log(event.target.value);
-    // });
-    const weatherForm = document.getElementById("weatherForm");
-    for (let i = 0; i < this.hourlyTime.length; i++) {
-      const newElement = document.createElement("div");
-      newElement.innerHTML += this.hourlyTime[i];
-      newElement.innerHTML += this.weatherCodeObject[this.weatherCode[i]];
-      weatherForm.appendChild(newElement);
+    const weatherTable = document.getElementById("weatherTable");
+    for (let i = 12; i < this.hourlyTime.length; i = i + 24) {
+      const newElement = document.createElement("tr");
+      const newDate = document.createElement("td");
+      newDate.innerText = this.hourlyTime[i].replace("T", "  ");
+      newElement.appendChild(newDate);
+      for (let j = 0; j < this.weatherCode.length; j++) {
+        const newWeather = document.createElement("td");
+        newWeather.innerText = this.weatherCodeObject[this.weatherCode[j][i]];
+        newElement.appendChild(newWeather);
+      }
+      weatherTable.appendChild(newElement);
     }
   }
 }
 
-//module.exports = WorldWeather;
 window.WorldWeather = WorldWeather;
-
-// let hourlyTime;
-// let weatherCode;
-
-// const weatherCodeObject = {
-//   0: "晴天",
-//   1: "晴れ",
-//   2: "一部曇り",
-//   3: "曇り",
-//   45: "霧",
-//   48: "濃い霧",
-//   51: "霧雨",
-//   53: "霧雨",
-//   55: "霧雨",
-//   56: "霧雨",
-//   57: "霧雨",
-//   61: "弱い雨",
-//   63: "雨",
-//   65: "強い雨",
-//   66: "霰",
-//   67: "霰",
-//   71: "弱い雪",
-//   73: "雪",
-//   75: "強い雪",
-//   99: "重度のひょうを伴う雷雨",
-//   96: "軽度のひょうを伴う雷雨",
-//   95: "雷雨",
-//   86: "重度のにわか雪",
-//   85: "軽度のにわか雪",
-//   82: "重度のにわか雨",
-//   81: "中度のにわか雨",
-//   80: "経度のにわか雨",
-//   77: "霧雪",
-// };
-
-// const serchWeather = async function () {
-//   await axios
-//     .get(
-//       "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m,weathercode"
-//     )
-//     .then(function (data) {
-//       hourlyTime = data.data.hourly.time;
-//       weatherCode = data.data.hourly.weathercode;
-//       console.log(hourlyTime);
-//       console.log(weatherCode);
-//       weatherCreatElement();
-//     });
-// };
-// serchWeather();
-
-// function weatherCreatElement() {
-//   const weatherForm = document.getElementById("weatherForm");
-//   for (let i = 0; i < hourlyTime.length; i++) {
-//     weatherForm.innerHTML += hourlyTime[i];
-//     weatherForm.innerHTML += weatherCodeObject[weatherCode[i]];
-//   }
-// }
--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,12 @@
 const expect = chai.expect;
-//import { WorldWeather } from "../index.js";
-//const WorldWeather = require("../index.js");
-
-//let pokemonager;
 
 describe("searchWeather", () => {
-  //   beforeEach(() => {
-  //     pokemonager = new Pokemonager();
-  //   });
-
   it("should return array", () => {
-    const test = new WorldWeather("Tokyo");
+    const test = new WorldWeather();
     //console.log("hourlyTime", typeof hourlyTime);
-    console.log(test.hourlyTime);
+    //console.log(test.hourlyTime);
     test.searchWeather();
+    //test.searchWeatherNewYork();
     expect(Array.isArray(test.hourlyTime)).to.equal(true);
   });
 });
-//   it("should create an array of user-facing review objects synchronously", () => {
-//     // Setup
-//     const reviewBuilder = new ReviewBuilder();
-
-//     // Exercise
-//     const actual = reviewBuilder.buildReviewsSync();
-
-//     // Assert
-//     expect(actual).to.deep.equal(expected);
-//   });
-// });
